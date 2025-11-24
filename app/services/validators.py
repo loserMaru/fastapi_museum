a2f64bf5d0fe98f239f120aef65ed88789e33685
Marlen <osmanov.m.i.20@gmail.com>
2025-11-24 07:18:33 +0300

    Email validator created

---
+++ app/services/validators.py
@@ -0,0 +1,10 @@
+from email_validator import validate_email, EmailNotValidError
+from fastapi import HTTPException
+
+def email_validate(email: str):
+    try:
+        email = validate_email(email, check_deliverability=False)
+        email = email.normalized
+    except EmailNotValidError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    return email
