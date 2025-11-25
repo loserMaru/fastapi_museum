bf3b0263638a2fd64a5f2e099ef901b2bc6b8d6f
Marlen <osmanov.m.i.20@gmail.com>
2025-11-25 12:24:03 +0300

    Checking is email exist, hashing password before creating new user. Delete request added

--- app/routers/user.py
+++ app/routers/user.py
@@ -11,8 +11,14 @@ from app.models.user_models import (
     UserUpdate,
 )
 from app.repositories.database import SessionDep
-from app.services.requests import get_list_from_db, get_item_from_db, update_item_from_db
-from app.services.validators import email_validate
+from app.services.requests import (
+    get_list_from_db,
+    get_item_from_db,
+    get_item_from_db_by_pk,
+    update_item_from_db,
+    delete_item_from_db,
+)
+from app.services.validators import email_validate, hash_password
 
 router = APIRouter(
     prefix="/user",
@@ -22,7 +28,7 @@ router = APIRouter(
 
 
 @router.get("/", response_model=list[UserPublic])
-async def read(
+async def get_list(
         session: SessionDep,
         offset: int = 0,
         limit: Annotated[int, Query(le=100)] = 100,
@@ -31,22 +37,27 @@ async def read(
 
 
 @router.get("/{user_id}", response_model=UserPublic)
-async def read_user(session: SessionDep, user_id: int):
+async def get_user(session: SessionDep, user_id: int):
     try:
-        return get_item_from_db(session, User, user_id)
+        return get_item_from_db_by_pk(session, User, user_id)
 
     except ItemNotFoundError:
         raise HTTPException(status_code=404, detail="User not found")
 
 
-@router.post("/", response_model=UserCreate)
+@router.post("/", response_model=UserPublic)
 async def create_user(user: UserCreate, session: SessionDep):
     db_user = User(**user.model_dump())
     email_validate(db_user.email)
-    session.add(db_user)
-    session.commit()
-    session.refresh(db_user)
-    return db_user
+    try:
+        get_item_from_db(session, User, "email", db_user.email)
+        raise HTTPException(status_code=400, detail="Email already exists")
+    except ItemNotFoundError:
+        db_user.password = hash_password(db_user.password)
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+        return db_user
 
 
 @router.patch("/{user_id}", response_model=UserUpdate)
@@ -59,3 +70,13 @@ async def update_user(session: SessionDep, user_id: int, user_data: UserUpdate):
 
     except ValidationError as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.delete("/{user_id}")
+async def delete_user(session: SessionDep, user_id: int):
+    try:
+        delete_item_from_db(session, User, user_id)
+        return {"msg": f"User with user id {user_id} deleted"}
+
+    except ItemNotFoundError:
+        raise HTTPException(status_code=404, detail="User not found")
