c83dba57f5360ec89026170e9ae61fbc7b44a386
Marlen <osmanov.m.i.20@gmail.com>
2025-11-25 12:23:20 +0300

    Hash password func

--- app/services/validators.py
+++ app/services/validators.py
@@ -1,5 +1,7 @@
 from email_validator import validate_email, EmailNotValidError
 from fastapi import HTTPException
+from passlib.context import CryptContext
+
 
 def email_validate(email: str):
     try:
@@ -8,3 +10,13 @@ def email_validate(email: str):
     except EmailNotValidError as e:
         raise HTTPException(status_code=400, detail=str(e))
     return email
+
+
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
