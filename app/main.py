08a073748358119e3d4921de59c7590de93a5702
Marlen <osmanov.m.i.20@gmail.com>
2025-11-26 15:02:12 +0300

    Category router import

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from app.repositories.database import create_db_and_tables
-from app.routers import user
+from app.routers import user, category
 
 app = FastAPI()
 
@@ -10,4 +10,6 @@ app = FastAPI()
 def on_startup():
     create_db_and_tables()
 
+
 app.include_router(user.router)
+app.include_router(category.router)
