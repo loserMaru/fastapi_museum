756eadbdced1e84b300437c0c9a30ebde6aacf25
Marlen <osmanov.m.i.20@gmail.com>
2025-11-30 23:09:37 +0300

    Decode token func

--- app/security/jwt_utils.py
+++ app/security/jwt_utils.py
@@ -1,12 +1,23 @@
 from datetime import datetime, timedelta
-from jose import jwt
+
+from fastapi import HTTPException
+from jose import jwt, JWTError
 
 SECRET_KEY = 'my-secret-key'
 ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def decode_token(token: str):
+    try:
+        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
