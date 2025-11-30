cf89e2524f0cc656dd48a3cd01c958cd06c83d89
Marlen <osmanov.m.i.20@gmail.com>
2025-11-30 23:07:48 +0300

    Auth router creation

---
+++ app/routers/auth.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, HTTPException, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app.exceptions.domain import ItemNotFoundError
+from app.models.auth_model import LoginRequest
+from app.models.user_models import User
+from app.repositories.database import SessionDep
+from app.security.jwt_utils import create_access_token
+from app.services.requests import get_item_from_db
+from app.services.validators import verify_password
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+
+@router.post("/login")
+async def login(
+        session: SessionDep,
+        data: LoginRequest
+):
+    try:
+        user = get_item_from_db(session, User, "email", data.email)
+
+    except ItemNotFoundError:
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+
+    if not verify_password(data.password, user.password):
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+
+    token = create_access_token({"sub": str(user.email)})
+    return {"access_token": token, "token_type": "bearer"}
