181796b40e6096c764e365712fec4ec7df3c3d28
Marlen <osmanov.m.i.20@gmail.com>
2025-12-13 19:08:58 +0300

    Exhibit create route test

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -1,3 +1,6 @@
+from app.tests.utils import assert_unauthorized
+
+
 def test_get_exhibits(client, auth_headers):
     response = client.get("/exhibit/", headers=auth_headers)
     assert response.status_code == 200
@@ -7,3 +10,16 @@ def test_get_exhibits(client, auth_headers):
 def test_get_exhibits_unauthorized(client):
     response = client.get("/exhibit/")  # без токена
     assert response.status_code == 403
+
+
+def test_create_exhibit_unauthorized(client):
+    assert_unauthorized(
+        client,
+        "/exhibit/",
+        "POST",
+        json={
+            "title": "test",
+            "description": "test",
+            "category_id": 1
+        }
+    )
