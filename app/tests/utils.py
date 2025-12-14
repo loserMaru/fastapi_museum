64bce0615286b92243862305bd22658d2ba36d6a
Marlen <osmanov.m.i.20@gmail.com>
2025-12-15 00:34:26 +0300

    Get list assert pattern

--- app/tests/utils.py
+++ app/tests/utils.py
@@ -4,6 +4,12 @@ def assert_unauthorized(client, url: str, method: str = "get", json: dict | None
     assert response.status_code in (401, 403)
 
 
+def get_list_assert(client, url: str, headers: dict):
+    response = client.get(url, headers=headers)
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+
 def assert_404(client, url, index, method: str = "get"):
     request = getattr(client, method.lower())
     response = request(f"{url}/{index}")
