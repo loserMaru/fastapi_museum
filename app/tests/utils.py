bd86c04cfc3b038d582b64064a51a290696b4157
Marlen <osmanov.m.i.20@gmail.com>
2025-12-14 18:02:16 +0300

    404 test pattern

--- app/tests/utils.py
+++ app/tests/utils.py
@@ -1,4 +1,10 @@
 def assert_unauthorized(client, url: str, method: str = "get", json: dict | None = None):
     request = getattr(client, method.lower())
-    response = request(url, json=json)
+    response = request(url)
     assert response.status_code in (401, 403)
+
+
+def assert_404(client, url, index, method: str = "get"):
+    request = getattr(client, method.lower())
+    response = request(f"{url}/{index}")
+    assert response.status_code == 404
