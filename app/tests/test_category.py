1034ae46c2ec7d7955afaf41fb08e600ae8f5388
Marlen <osmanov.m.i.20@gmail.com>
2025-12-16 16:42:02 +0300

    Test create category

--- app/tests/test_category.py
+++ app/tests/test_category.py
@@ -4,5 +4,22 @@ from app.tests.utils import assert_404, get_list_assert
 def test_get_list_category(client):
     get_list_assert(client, "/category/", None)
 
+
 def test_nonexisting_category(client):
     assert_404(client, "/category/", 555)
+
+
+def test_post_category(client, auth_headers):
+    payload = {
+        "title": "Test title",
+        "description": "Test description"
+    }
+
+    response = client.post("/category/", json=payload)
+
+    assert response.status_code == 200
+
+    # Дополнительная проверка что экземпляр действительно был создан
+    data = response.json()
+    assert data["id"]
+    assert data["title"] == payload["title"]
