d4f9739953b9cc2054072383bd5b293fe340781d
Marlen <osmanov.m.i.20@gmail.com>
2025-12-16 16:36:23 +0300

    Test get list category

--- app/tests/test_category.py
+++ app/tests/test_category.py
@@ -1,5 +1,8 @@
-from app.tests.utils import assert_404
+from app.tests.utils import assert_404, get_list_assert
 
+
+def test_get_list_category(client):
+    get_list_assert(client, "/category/", None)
 
 def test_nonexisting_category(client):
     assert_404(client, "/category/", 555)
